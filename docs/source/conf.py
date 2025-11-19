62d326be42b9ba8f7096ba59ab04f10a799ccc48
Uma-Obbani <your@maheswaree@gmail.com>
2025-11-19 13:09:24 +0100

    rework

---
+++ docs/source/conf.py
@@ -0,0 +1,47 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+# Add these three lines near the beginning of the file to find the project root
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+project = 'python_testing_docs'
+copyright = '2025, Uma'
+author = 'Uma'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [   'Sphinx.ext.autodoc',
+   'sphinx.ext.autosummary',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages',
+    'sphinx.ext.coverage',
+    'sphinx.ext.napoleon', # Supports numpy and google style docstrings
+    'sphinx_rtd_theme',    # Required for the Read the Docs theme
+    'sphinx_autodoc_typehints' # Uses Python type hints in the docs
+
+    ]
+
+
+
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+# Set the HTML theme (around line 170)
+html_theme = 'sphinx_rtd_theme'
+
+# html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,17 @@
+.. python_testing_docs documentation master file, created by
+   sphinx-quickstart on Wed Nov 19 12:18:15 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+calculator documentation
+========================
+.. automodule:: calculator.calculator
+   :members:
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+* :ref:`getindex`
+* :ref:`modindex`
+* :ref:`search`
